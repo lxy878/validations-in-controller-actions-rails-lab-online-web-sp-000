f6fd34fbffb53db535bf41b070929694eda001b6
Xiaoyi Liu <lxy878@gmail.com>
2020-06-21 11:04:34 +0000

    Automatically backed up by Learn

--- app/models/category_validator.rb
+++ app/models/category_validator.rb
@@ -1,7 +1,7 @@
 class CategoryValidator < ActiveModel::Validator
   def validate(record)
     unless record.category="Fiction" || record.category="Non-Fiction"
-      record.errors[:category] << 'Either "Fiction" or "Non-Fiction"'
+      record.errors[:category] << 'Choose either "Fiction" or "Non-Fiction"'
     end
   end
 end
