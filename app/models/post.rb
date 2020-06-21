47aeca8e770610c32bf94ce0d6ad03ccf0add72f
Xiaoyi Liu <lxy878@gmail.com>
2020-06-21 10:23:54 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
-  validates :content, presence: true
-  validates :category, presence: true
+  include ActiveModel::validations
+  
 end
