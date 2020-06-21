10b374505302c6d18946d37eb854cf1e60c57d53
Xiaoyi Liu <lxy878@gmail.com>
2020-06-21 11:27:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 100}
-  include ActiveRecord::Validations
-  validates_with CategoryValidator
+  validates :category, inclusion: {in: %w{Fiction Non-Fiction}}
 end
