0387657359234d3686023c077c94e927cc468892
Xiaoyi Liu <lxy878@gmail.com>
2020-06-21 10:58:03 +0000

    Automatically backed up by Learn

--- app/models/category_validator.rb
+++ app/models/category_validator.rb
@@ -1,7 +1,7 @@
 class CategoryValidator
   def validate(record)
     unless record.category="Fiction" || record.category="Non-Fiction"
-      record.errors[:category] << ''
+      record.errors[:category] << 'either "Fiction" or "Non-Fiction"'
     end
   end
 end
