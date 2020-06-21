8ea3dab73117ef5a91e20c490c2597b646594039
Xiaoyi Liu <lxy878@gmail.com>
2020-06-21 10:28:26 +0000

    Automatically backed up by Learn

--- app/models/validator.rb
+++ app/models/validator.rb
@@ -1,10 +1,10 @@
 class Validator
   def validate_each(record, attributes, value)
     unless value.empty?
-
+      record.errors[attributes] << "empty" 
     end
   end
-  
+
   def validate(record)
 
   end
